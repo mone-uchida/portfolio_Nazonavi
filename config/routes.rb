58eb1937bf438c996e0bbabd7445111f3aba85cf
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-09-16 15:46:40 +0900

    トップ画面とユーザーに関するルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/',  to: 'home#top'
+  get '/home', to: 'home#index'
+  get '/login', to: 'users#login'
+  resources :users, only: [:new]
 end
