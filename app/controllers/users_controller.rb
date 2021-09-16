8c750622a965a8f1cec3a5af7f513b4bb7094b4c
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-09-16 16:51:19 +0900

    ユーザー登録機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.icon = 'default_user.jpg'
+    @user.profile = '未設定'
+    if @user.save
+      flash[:notice] = 'アカウントを作成しました'
+      redirect_to '/home'
+    else
+      flash[:notice] = '入力内容にエラーがあります'
+      render new_user_path
+    end
   end
 
   def login
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password_digest, :icon, :profile)
   end
 end
