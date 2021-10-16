c71919e6ba3caaf61155733584a3927ae45366dc
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-10-16 15:11:42 +0900

    管理画面へのルート作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   get '/',         to: 'home#top'
   get '/home',     to: 'home#index'
   get '/login',    to: 'users#login_form'
