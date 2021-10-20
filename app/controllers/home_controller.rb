71b58aace7cad3b1174d5e50c6e39bbf1a2f1685
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-10-20 14:04:26 +0900

    gmapへのデータ受け渡しを実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,10 @@ class HomeController < ApplicationController
   end
 
   def index
+    @events = Event.where("finish_at >= ? OR finish_at = ?", Date.today, [])
+    gon.events = @events
+    gon.spots = Spot.all.select(:lat, :lng)
+    gon.titles = Title.all.select(:name, :image)
+    gon.s3_url = ENV['S3_URL']
   end
 end
