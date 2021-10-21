97c3b39a9d5c3eaffaeddcdb6615d8b15567fa7c
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-10-21 18:39:42 +0900

    resources eventsの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'events/index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get '/',         to: 'home#top'
   get '/home',     to: 'home#index'
   get '/login',    to: 'users#login_form'
   post '/login',   to: 'users#login'
+  post '/logout' => 'users#logout'
   resources :users
+  resources :events
 end
