8e29bb0c2f0dbc5e3011220932faf2121080ab50
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-10-23 17:43:29 +0900

    ログイン・ログアウト機能の修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   get 'events/index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get '/',         to: 'home#top'
-  get '/home',     to: 'home#index'
-  get '/login',    to: 'users#login_form'
-  post '/login',   to: 'users#login'
-  post '/logout',  to: 'users#logout'
+  get '/',            to: 'home#top'
+  get '/home',        to: 'home#index'
+  get '/login',       to: 'users#login_form'
+  post '/login',      to: 'users#login'
+  delete '/logout',   to: 'users#destroy'
   resources :users
   resources :events
 end
