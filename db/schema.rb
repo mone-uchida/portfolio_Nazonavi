297891792ae96f68ad8db8bc7529e9c20dee128d
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-10-26 16:47:40 +0900

    お気に入り情報のデータベースを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_21_090419) do
+ActiveRecord::Schema.define(version: 2021_10_26_074248) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -47,11 +47,18 @@ ActiveRecord::Schema.define(version: 2021_10_21_090419) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "event_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "spots", force: :cascade do |t|
     t.string "place"
     t.string "address"
-    t.string "lat"
-    t.string "lng"
+    t.float "lat"
+    t.float "lng"
     t.string "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
