a8cd09d5da715f5648ca71c9fb14f5d1d28de374
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-10-27 17:27:29 +0900

    favorites関連のルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get '/',            to: 'home#top'
-  get '/home',        to: 'home#index'
-  get '/login',       to: 'users#login_form'
-  post '/login',      to: 'users#login'
-  delete '/logout',   to: 'users#destroy'
+  get '/',                to: 'home#top'
+  get '/home',            to: 'home#index'
+  get '/login',           to: 'users#login_form'
+  post '/login',          to: 'users#login'
+  delete '/logout',       to: 'users#destroy'
+  post 'favorite/:id',    to: 'favorites#create', as: 'create_favorite'
+  delete 'favorite/:id',  to: 'favorites#destroy', as: 'destroy_favorite'
   resources :users
   resources :events, only: [:index, :show]
   resources :spots, only: [:index, :show]
