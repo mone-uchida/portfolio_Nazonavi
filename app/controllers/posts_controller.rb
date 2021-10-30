79441a6ae1c4616bc09c5504af4cf7d5a4075892
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-10-30 22:37:53 +0900

    詳細ページ、編集・削除機能の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,11 +13,39 @@ class PostsController < ApplicationController
     @post.user = current_user
     if @post.save
       flash[:notice]="投稿が完了しました"
-      redirect_to user_posts_path(current_user)
+      redirect_to users_posts_index_path(current_user)
     else
       flash[:notice]="入力内容にエラーがあります"
-      render ("posts/new")
+      render new_post_path
     end
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end    
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    if @post.user == current_user && @post.update(post_params)
+      flash[:notice]="編集が完了しました"
+      redirect_to users_posts_index_path(current_user)
+    elsif @post.user == current_user
+      flash[:notice]="入力内容にエラーがあります"
+      render users_posts_index_path(current_user)
+    else
+      flash[:notice]="他のユーザーの投稿のため、編集できません"
+      redirect_to posts_path
+    end
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to users_posts_index_path(current_user)
   end
 
   private
