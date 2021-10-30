3aec21c31a64fe7d193d511e1227ff3581ba6474
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-10-30 22:41:56 +0900

    titleテーブルとの関連付けを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :title
 
+  delegate :name, to: :title
+
   with_options presence: true do
     validates :user_id
     validates :title_id
