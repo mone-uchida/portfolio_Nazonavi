5029c73036b2c55ca86c0a35fe3722b77d0774be
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-10-30 17:09:35 +0900

    postcontrollerの作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -9,17 +9,16 @@ class FavoritesController < ApplicationController
 
   def create
     favorite = Favorite.create(user_id: current_user.id, event_id: params[:id])
-    favorite.save
   end
 
   def destroy
     favorite = Favorite.find_by(user_id: current_user.id, event_id: params[:id])
     favorite.destroy
-    favorite.save
   end
   
   
   private
+  
   def set_event
     @event = Event.find_by(id: params[:id])
   end
