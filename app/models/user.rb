e82fd9c9854c10492d803b7b4320dbb5778fd448
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-10-30 17:04:44 +0900

    postテーブルとの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
+
   has_many :events
   has_many :favorites
+  has_many :posts
+
   validates :name, {presence: true}
   validates :email, {presence: true, uniqueness: true}
+  
   def favorite_find(event_id)
     favorites.where(event_id: event_id).exists?
   end
