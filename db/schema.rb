93580c1e32e1ea90de5270d7d4038e99e027c8c4
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-10-30 18:03:02 +0900

    postテーブルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_27_082027) do
+ActiveRecord::Schema.define(version: 2021_10_28_141226) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -53,6 +53,14 @@ ActiveRecord::Schema.define(version: 2021_10_27_082027) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id", "event_id"], name: "index_favorites_on_user_id_and_event_id", unique: true
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "title_id"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "spots", force: :cascade do |t|
