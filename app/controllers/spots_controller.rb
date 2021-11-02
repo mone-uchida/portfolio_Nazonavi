d4a23c01fde72897c531607883ac2313c3e0dc70
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-11-02 18:26:48 +0900

    会場の詳細ページにgooglemapを表示

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -2,5 +2,6 @@ class SpotsController < ApplicationController
   def show
     @spot = Spot.find_by(id: params[:id])
     @related_events = Event.where(spot_id: params[:id])
+    gon.spot = @spot
   end
 end
