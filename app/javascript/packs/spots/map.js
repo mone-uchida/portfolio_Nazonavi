0c36acda9ac1fcfaa8bc115b3f5f319feb0ab269
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-11-02 18:28:29 +0900

    会場のマップとピンを表示する機能を実装

---
+++ app/javascript/packs/spots/map.js
@@ -0,0 +1,16 @@
+let map;
+let marker;
+let num = 13;
+let spot = gon.spot;
+let Lat = spot.lat;
+let Lng = spot.lng;
+window.initMap = function(){
+  map = new google.maps.Map(document.getElementById('spot-map'), {
+      center: {lat: Lat,lng: Lng},
+      zoom: num
+  }); 
+  marker = new google.maps.Marker({
+    position: {lat: Lat, lng: Lng},
+    map: map
+  })
+}
