17a3142f32cf6be9e8ff86fdcfd89a7c740367f3
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-11-04 13:57:29 +0900

    ページタイトルの作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  BASETITLE = 'なぞなび'.freeze
+  def title(page_title)
+    if page_title.empty?
+      BASETITLE
+    else
+      "#{page_title} | #{BASETITLE}"
+    end
+  end
 end
