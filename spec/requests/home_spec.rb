fcb81c2b4561e4ac84d56e46fa4cb0112e54eb2b
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-11-15 11:21:24 +0900

    home_specの作成

---
+++ spec/requests/home_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'Home', type: :request do
+  let(:user) { create(:user) }
+  let(:title) { create(:title) }
+  let(:post) { create(:post) }
+
+  describe "Home" do
+    describe "ゲストユーザー" do
+      it 'GET top' do
+        get "/"
+        expect(response).to have_http_status(200)
+      end
+
+      it 'GET index' do
+        get home_path
+        expect(response).to have_http_status(200)
+        expect(response.body).to_not include "投稿する"
+      end
+    end
+    describe "ログインユーザー" do
+      before do
+        allow_any_instance_of(ActionDispatch::Request).
+          to receive(:session).
+          and_return({ user_id: user.id })
+      end
+      it 'GET index' do
+        get home_path
+        expect(response).to have_http_status(200)
+        expect(response.body).to include "投稿する"
+      end
+    end
+  end
+end
