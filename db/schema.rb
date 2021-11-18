2b4bf2873e85f755a57d3830abd3bf28700170cd
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-11-18 18:34:39 +0900

    誤って消去したテーブルを再設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_28_141226) do
+ActiveRecord::Schema.define(version: 2021_11_18_092249) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
