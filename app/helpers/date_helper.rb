07d7e1044b691e0623ec300f10f4facae3ee40a8
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-11-18 15:39:52 +0900

    日時表記のヘルパーを作成

---
+++ app/helpers/date_helper.rb
@@ -0,0 +1,22 @@
+module DateHelper
+
+  def start_at_show(event)
+    if event.start_at.present?
+      event.start_at.strftime('%Y年%m月%d日')
+    else
+      "-"
+    end
+  end
+
+  def finish_at_show(event)
+    if event.finish_at.present?
+      event.start_at.strftime('%Y年%m月%d日')
+    else
+      "-"
+    end
+  end
+
+  def created_at_show(post)
+    post.created_at.strftime('%m月%d日 %H:%M')
+  end
+end
