7cde6e59f4957898a10a60e94ac58dc074a57a3d
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-11-19 12:36:06 +0900

    重複したユーザーのデータ作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,4 +5,11 @@ FactoryBot.define do
     password_digest { 'password' }
     icon { 'default_user.jpg' }
   end
+
+  factory :duplication_user, class: User do
+    name { 'test' }
+    email { "test@example.com" }
+    password_digest { 'password' }
+    icon { 'default_user.jpg' }
+  end
 end
