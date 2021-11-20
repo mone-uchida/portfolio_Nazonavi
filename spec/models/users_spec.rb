fadf29f14efcc1f4ae36e264d783c7348fbbbcc1
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-11-20 12:19:55 +0900

    テストのチェック項目を追加

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -11,37 +11,50 @@ RSpec.describe User, type: :model do
   describe "登録に失敗" do
     describe "名前が無効" do
       it "名前がない" do
-        expect(build(:user, name: nil)).to_not be_valid
+        user = build(:user, name: nil)
+        user.valid?
+        expect(user.errors[:name]).to include("can't be blank")
       end
 
       it "名前が20文字以上ある" do
-        expect(build(:user, name: ("a" * 21))).to_not be_valid
+        user = build(:user, name: ("a" * 21))
+        user.valid?
+        expect(user.errors[:name]).to include("is too long (maximum is 20 characters)")
       end
     end
 
     describe "メールアドレスが無効" do
       it "メールアドレスがない"  do
-        expect(build(:user, email: nil)).to_not be_valid
+        user = build(:user, email: nil)
+        user.valid?
+        expect(user.errors[:email]).to include("can't be blank")
       end
 
       it "重複している" do
         user1 = create(:duplication_user)
-        user2 = buildrspec ./spec/models/users_spec.rb(:duplication_user)
-        expect(user2).to_not be_valid
+        user2 = build(:duplication_user)
+        user2.valid?
+        expect(user2.errors[:email]).to include("has already been taken")
       end
 
       it "aaa@aaa.aaaの形式でない" do
-        expect(build(:user, email: "a")).to_not be_valid
+        user = build(:user, email: "a")
+        user.valid?
+        expect(user.errors[:email]).to include("is invalid")
       end
 
       it "50文字以上ある" do
-        expect(build(:user, name: ("a" * 48 + ".com"))).to_not be_valid
+        user = build(:user, email: ("a" * 48 + ".com"))
+        user.valid?
+        expect(user.errors[:email]).to include("is too long (maximum is 50 characters)")
       end
     end
 
     describe "パスワードが無効" do
       it "パスワードがない場合、無効である" do
-        expect(build(:user, password_digest: "")).to_not be_valid 
+        user = build(:user, password_digest: nil)
+        user.valid?
+        expect(user.errors[:password_digest]).to include("can't be blank")
       end
     end
   end
