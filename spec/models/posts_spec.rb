69850b18a81ea2f80d12f0408465f47a9bca85e9
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-11-20 11:55:32 +0900

    不要な記述の削除

--- spec/models/posts_spec.rb
+++ spec/models/posts_spec.rb
@@ -5,18 +5,18 @@ RSpec.describe Post, type: :model do
   let!(:title) { create(:title) }
 
   it "タイトル、投稿内容がある場合、有効である" do
-    post = FactoryBot.build(:post)
+    post = build(:post)
     expect(post).to be_valid
   end
 
   it "タイトルがない場合、無効である" do
-    post = FactoryBot.build(:post, title_id: nil)
+    post = build(:post, title_id: nil)
     post.valid?
     expect(post.errors[:title]).to include("must exist")
   end
 
   it "投稿内容がない場合、無効である"  do
-    post = FactoryBot.build(:post, content: nil)
+    post = build(:post, content: nil)
     post.valid?
     expect(post.errors[:content]).to include("can't be blank")
   end
