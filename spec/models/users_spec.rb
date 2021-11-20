a585f615d74e2d744716f0581b95199c967ee22b
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-11-20 11:51:46 +0900

    テストの内容を修正

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,33 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "名前、メール、パスワードがある場合、有効である" do
-    user = FactoryBot.build(:user)
-    expect(user).to be_valid
+  describe "登録に成功" do
+    it "名前、メール、パスワードがある場合、有効である" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
   end
 
-  it "名前がない場合、無効である" do
-    user = FactoryBot.build(:user, name: nil)
-    user.valid?
-    expect(user.errors[:name]).to include("can't be blank")
-  end
+  describe "登録に失敗" do
+    describe "名前が無効" do
+      it "名前がない" do
+        expect(build(:user, name: nil)).to_not be_valid
+      end
 
-  it "メールアドレスがない場合、無効である"  do
-    user = FactoryBot.build(:user, email: nil)
-    user.valid?
-    expect(user.errors[:email]).to include("can't be blank")
-  end
+      it "名前が20文字以上ある" do
+        expect(build(:user, name: ("a" * 21))).to_not be_valid
+      end
+    end
 
-  it "重複したメールアドレスの場合、無効である" do
-    user1 = FactoryBot.create(:duplication_user)
-    user2 = FactoryBot.build(:duplication_user)
-    user2.valid?
-    expect(user2.errors[:email]).to include("has already been taken")
-  end
+    describe "メールアドレスが無効" do
+      it "メールアドレスがない"  do
+        expect(build(:user, email: nil)).to_not be_valid
+      end
 
-  it "パスワードがない場合、無効である" 
-    user = FactoryBot.build(:user, password_digest: nil)
-    user.valid?
-    expect(user.errors[:password_digest]).to include("can't be blank")
+      it "重複している" do
+        user1 = create(:duplication_user)
+        user2 = buildrspec ./spec/models/users_spec.rb(:duplication_user)
+        expect(user2).to_not be_valid
+      end
+
+      it "aaa@aaa.aaaの形式でない" do
+        expect(build(:user, email: "a")).to_not be_valid
+      end
+
+      it "50文字以上ある" do
+        expect(build(:user, name: ("a" * 48 + ".com"))).to_not be_valid
+      end
+    end
+
+    describe "パスワードが無効" do
+      it "パスワードがない場合、無効である" do
+        expect(build(:user, password_digest: "")).to_not be_valid 
+      end
+    end
   end
 end
