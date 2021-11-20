f2ab263d9421ea7af61965667f95063580a4834c
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-11-20 18:12:27 +0900

    データ作成の優先度を変更

--- spec/models/favorites_spec.rb
+++ spec/models/favorites_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  let(:user) { create(:user) }
-  let(:title) { create(:title) }
+  let!(:user) { create(:user) }
+  let!(:title) { create(:title) }
+  let!(:spot) { create(:spot) }
+  let!(:event) { create(:event) }
 
   it "タイトル名がある場合、有効である" do
-    favorite = build(:favorite, user_id: 1, title_id: 1)
+    favorite = build(:favorite, user_id: 1, event_id: 1)
     expect(favorite).to be_valid
   end
 
