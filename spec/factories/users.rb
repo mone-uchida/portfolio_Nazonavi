5e0950e2991859f6177aec7f8c81b1021a403cd5
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-11-21 18:37:41 +0900

    rubocopの指摘内容を修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,7 +8,7 @@ FactoryBot.define do
 
   factory :duplication_user, class: User do
     name { 'test' }
-    email { "test@example.com" }
+    email { 'test@example.com' }
     password_digest { 'password' }
     icon { 'default_user.jpg' }
   end
