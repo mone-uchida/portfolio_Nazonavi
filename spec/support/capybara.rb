c67f4b24098fb4d773b889c02adde12ea40607f6
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-11-25 11:56:43 +0900

    circleCIでも通るように設定変更

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -4,21 +4,17 @@ require 'selenium-webdriver'
 RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :remote_chrome
-    Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
-    Capybara.server_port = 3001
-    Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
   end
 
   config.before(:each, type: :system, js: true) do
     driven_by :remote_chrome
-    Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
-    Capybara.server_port = 3002
-    Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
   end
 end
 
+Capybara.default_driver    = :remote_chrome
+Capybara.javascript_driver = :remote_chrome
+
 Capybara.register_driver :remote_chrome do |app|
-  url = 'http://chrome:4444/wd/hub'
   caps = ::Selenium::WebDriver::Remote::Capabilities.chrome(
     'goog:chromeOptions' => {
       'args' => [
@@ -30,5 +26,5 @@ Capybara.register_driver :remote_chrome do |app|
       ]
     }
   )
-  Capybara::Selenium::Driver.new(app, browser: :remote, url: url, capabilities: caps)
+  Capybara::Selenium::Driver.new(app, browser: :chrome, capabilities: caps)
 end
