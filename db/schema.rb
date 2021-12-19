154afe82d9dcffac6131b4f7ade236a9bf03cd11
内田望音 <uchidamone@mone-MacBook-Air.local>
2021-12-19 23:44:50 +0900

    データベースをmysqlへ変更

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2021_11_20_021519) do
 
-  create_table "active_admin_comments", force: :cascade do |t|
+  create_table "active_admin_comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
     t.string "resource_type"
-    t.integer "resource_id"
+    t.bigint "resource_id"
     t.string "author_type"
-    t.integer "author_id"
+    t.bigint "author_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2021_11_20_021519) do
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
   end
 
-  create_table "admin_users", force: :cascade do |t|
+  create_table "admin_users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2021_11_20_021519) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
-  create_table "events", force: :cascade do |t|
+  create_table "events", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "spot_id"
     t.integer "title_id"
     t.date "start_at"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 2021_11_20_021519) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "favorites", force: :cascade do |t|
+  create_table "favorites", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "user_id"
     t.integer "event_id"
     t.datetime "created_at", precision: 6, null: false
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 2021_11_20_021519) do
     t.index ["user_id", "event_id"], name: "index_favorites_on_user_id_and_event_id", unique: true
   end
 
-  create_table "posts", force: :cascade do |t|
+  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "user_id"
     t.integer "title_id"
     t.text "content"
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 2021_11_20_021519) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "spots", force: :cascade do |t|
+  create_table "spots", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "place"
     t.string "address"
     t.float "lat"
@@ -73,7 +73,7 @@ ActiveRecord::Schema.define(version: 2021_11_20_021519) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "titles", force: :cascade do |t|
+  create_table "titles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "image"
     t.integer "price"
@@ -85,7 +85,7 @@ ActiveRecord::Schema.define(version: 2021_11_20_021519) do
     t.integer "time"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
