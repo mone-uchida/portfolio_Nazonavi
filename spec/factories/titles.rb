ddbcbeb05877cdcf12f3a501b79239518ebb9510
内田望音 <uchidamone@mone-MacBook-Air.local>
2022-01-16 17:35:22 +0900

    テストデータの追加

--- spec/factories/titles.rb
+++ spec/factories/titles.rb
@@ -2,15 +2,18 @@ FactoryBot.define do
   factory :title, class: Title do
     id { 1 }
     name { 'なぞとき' }
+    image { 'default_event_image.jpg' }
   end
 
   factory :other_title, class: Title do
     id { 2 }
     name { '脱出' }
+    image { 'default_event_image.jpg' }
   end
 
   factory :finished_title, class: Title do
     id { 3 }
     name { '宝探し' }
+    image { 'default_event_image.jpg' }
   end
 end
