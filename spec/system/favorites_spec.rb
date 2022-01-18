1bf5caaacf51383c846bf4615cb9d2469596e38b
内田望音 <uchidamone@mone-MacBook-Air.local>
2022-01-18 15:43:50 +0900

    いいね一覧の表示に関するテストを追加

--- spec/system/favorites_spec.rb
+++ spec/system/favorites_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe Favorite, type: :system, js: true do
   let(:event) { create(:event) }
   let!(:spot) { create(:spot) }
   let!(:title) { create(:title) }
+  let!(:other_spot) { create(:other_spot) }
+  let!(:other_title) { create(:other_title) }
+  let!(:other_event) { create(:other_event) }
 
   describe 'Favorite' do
     describe 'いいね機能' do
@@ -26,6 +29,21 @@ RSpec.describe Favorite, type: :system, js: true do
             expect(page).to have_css '#favorite-destroy'
             find('#favorite-destroy').click
             expect(page).to have_css '#favorite-create'
+          end
+        end
+        describe 'いいね一覧' do
+          before do
+            visit event_path(event)
+            expect(page).to have_css '#favorite-destroy'
+            visit user_favorites_path(user)
+          end
+
+          it 'いいねしたイベントが表示される' do
+            expect(page).to have_content title.name
+          end
+
+          it 'いいねしていないイベントが表示されない' do
+            expect(page).to_not have_content other_title.name
           end
         end
       end
