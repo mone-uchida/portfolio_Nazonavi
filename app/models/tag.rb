b96b12802b6e1b617822a11b2916fc4c4072ecbb
内田望音 <uchidamone@mone-MacBook-Air.local>
2022-05-14 19:03:56 +0900

    不要な記述の削除

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,5 @@
 class Tag < ApplicationRecord
   has_many :event_tags, dependent: :destroy, foreign_key: 'tag_id'
-  # タグは複数の投稿を持つ　それは、event_tagsを通じて参照できる
   has_many :events, through: :event_tags
   validates :name, uniqueness: true, presence: true
 end
